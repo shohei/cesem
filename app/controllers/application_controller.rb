a5d3a45d4ea716fbea9f7850f7474fdaa95b6fb4
Shohei Aoki <shoaok@gmail.com>
2019-06-11 22:15:40 +0300

    implement login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
     if user_signed_in?
       super
     else
-      redirect_to root_path, :notice => 'login before using cesem system'
+      redirect_to '/users/sign_in'
       ## if you want render 404 page
       ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
     end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   root 'home#index'
+  # devise_scope :user do
+  #   root to: "devise/sessions#new"
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
