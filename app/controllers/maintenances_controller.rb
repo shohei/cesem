e012fb5cf634d327e39b6b75139998d99d7adb0a
Shohei Aoki <shoaok@gmail.com>
2019-06-19 16:23:59 +0300

    add acknowledgement page

--- app/controllers/maintenances_controller.rb
+++ app/controllers/maintenances_controller.rb
@@ -2,6 +2,7 @@ class MaintenancesController < ApplicationController
   before_action :set_maintenance, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
+  require 'date'
 
   # GET /maintenances
   # GET /maintenances.json
@@ -28,10 +29,13 @@ class MaintenancesController < ApplicationController
   # POST /maintenances.json
   def create
     @maintenance = Maintenance.new(maintenance_params)
+    trace_number = generate_trace_number(@maintenance)
+    @maintenance.trace_number = trace_number
+    inventory_number = Equipment.find_by_id(@maintenance.equipment_id).inventory_number 
 
     respond_to do |format|
       if @maintenance.save
-        format.html { redirect_to :acknowledgement }
+        format.html { redirect_to "/acknowledgement/?trace_number=#{trace_number}&inventory_number=#{inventory_number}" }
         format.json { render :show, status: :created, location: @maintenance }
       else
         format.html { render :new }
@@ -65,7 +69,8 @@ class MaintenancesController < ApplicationController
   end
 
   def acknowledgement 
-
+    @trace_number = params[:trace_number]
+    @inventory_number = params[:inventory_number]
   end
 
   def followup
@@ -73,6 +78,14 @@ class MaintenancesController < ApplicationController
   end
 
   private
+    def generate_trace_number(maintenance)
+      year = Date.today.year.to_s
+      month = format('%02d', Date.today.month)
+      four_digit = format('%04d', Maintenance.count)
+      trace_number = "CeSEM/"+month+"/"+four_digit+"/"+year
+      return trace_number
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_maintenance
       @maintenance = Maintenance.find(params[:id])
