c839978b80449f32722f0f6328df75a6e0bb0435
Shohei Aoki <shoaok@gmail.com>
2022-11-08 21:28:37 +0300

    Bump to Rails 7.0.4 & Ruby 3.1.2

--- db/schema.rb
+++ db/schema.rb
@@ -2,30 +2,26 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_094459) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
+ActiveRecord::Schema[7.0].define(version: 2019_07_24_094459) do
   create_table "billings", force: :cascade do |t|
     t.integer "maintenance_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "colleges", force: :cascade do |t|
     t.string "name"
     t.string "abbreviation"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "dealers", force: :cascade do |t|
@@ -33,8 +29,8 @@ ActiveRecord::Schema.define(version: 2019_07_24_094459) do
     t.string "phone_number"
     t.string "address"
     t.string "contact_person_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "departments", force: :cascade do |t|
@@ -42,8 +38,8 @@ ActiveRecord::Schema.define(version: 2019_07_24_094459) do
     t.string "name"
     t.string "abbreviation"
     t.string "alias"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "equipments", force: :cascade do |t|
@@ -58,7 +54,7 @@ ActiveRecord::Schema.define(version: 2019_07_24_094459) do
     t.string "manufacturer"
     t.string "country_of_origin"
     t.string "warranty_period"
-    t.integer "additional_info", default: [], array: true
+    t.integer "additional_info"
     t.text "other_info"
     t.integer "dealer_id"
     t.integer "internal_number"
@@ -70,16 +66,16 @@ ActiveRecord::Schema.define(version: 2019_07_24_094459) do
     t.integer "department_id"
     t.boolean "archived", default: false
     t.integer "status", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "items", force: :cascade do |t|
     t.integer "service_id"
     t.string "name"
     t.integer "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "maintenances", force: :cascade do |t|
@@ -95,8 +91,8 @@ ActiveRecord::Schema.define(version: 2019_07_24_094459) do
     t.date "recommission_projected_at"
     t.date "completed_at"
     t.text "other_status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.date "canceled_at"
     t.date "cancel_requested_at"
   end
@@ -105,29 +101,29 @@ ActiveRecord::Schema.define(version: 2019_07_24_094459) do
     t.integer "college_id"
     t.string "name"
     t.string "abbreviation"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "services", force: :cascade do |t|
     t.integer "billing_id"
     t.integer "amount"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
+    t.datetime "confirmed_at", precision: nil
+    t.datetime "confirmation_sent_at", precision: nil
     t.string "unconfirmed_email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "name", null: false
     t.integer "role", default: 2, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
